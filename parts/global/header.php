10b846461a0780864731882cf56951ecd0383766
Dana Malkin <dana@danamalkin.com>
2018-01-20 11:25:50 -0700

    complete mobile menu

--- parts/global/header.php
+++ parts/global/header.php
@@ -11,7 +11,7 @@
 ?>
 
 <header class="container header">
-  <div class="row row--align-items-center">
+  <div class="row row--align-items-center row--justify-content-center">
 
       <div class="col-4 sm-hide">
         <?php // Primary Menu (Left)
@@ -29,7 +29,7 @@
         ?>
       </div>
 
-      <div class="moble-logo sm-only">
+      <div class="mobile-logo sm-only">
         <a href="<?php echo home_url(); ?>">
           <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-sm.svg" alt="Allie Malkin Creative" />
         </a>
@@ -42,7 +42,9 @@
 
   <div class="nifty-panel">
     <?php // Primary Menu
-    wp_nav_menu( array('theme_location' => 'primary', 'container' => 'nav', 'container_class' => 'primary-nav') ); ?>
+      wp_nav_menu( array('theme_location' => 'primary-left', 'container' => 'nav', 'container_class' => 'mobile-nav') );
+      wp_nav_menu( array('theme_location' => 'primary-right', 'container' => 'nav', 'container_class' => 'mobile-nav') );
+    ?>
   </div>
 
 </header>
